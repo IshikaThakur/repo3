8b604e1b3f77a8111bb1d2119f444f60397f3e89
IshikaThakur <60641237+IshikaThakur@users.noreply.github.com>
2020-03-03 13:20:15 +0530

    Update Q3_method.java

--- MultiThreading/Q3_method.java
+++ MultiThreading/Q3_method.java
@@ -1,35 +1,39 @@
-package MultiThreading;
-// 4. Write a program using synchronization block and synchronization method
-public class Q3_method {
-    public static int i=0;
-    public static void main(String[] args) throws InterruptedException{
-        Sync obj=new Sync();
-        Testing t1=new Testing(obj);
-        Testing t2=new Testing(obj);
-        t1.start();
-        t2.start();
+//  Using Synchronized Method
+package Multithreading;
+class Counter
+{
+     int count;
+    public synchronized void increment()
+    {
+        count=count+1;
     }
 }
-class Sync {
-       synchronized void Synching(){
-        for(Q3_method.i=0; Q3_method.i<10; Q3_method.i++){
-            System.out.println("i= "+ Q3_method.i);
-        }
-        try {
-            Thread.sleep(400);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
-class Testing extends Thread{
-    Sync sync;
-    Testing(Sync sync){
-        this.sync=sync;
-    }
-    public void run(){
-        sync.Synching();
-    }
-}
+public class SynchronizedConcept  {
+    public static void main(String[] args) throws Exception{
+        {
+            Counter c=new Counter();
+            Thread t1=new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i=0;i<100;i++)
+                    {
+                        c.increment();
+                }}
+            });
+            Thread t2=new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i=0;i<100;i++)
+                    {
+                        c.increment();
+                    }}
+            });
+            t1.start();
+            t2.start();
+           t1.join();
+           t2.join();
 
-
+            System.out.println("Count" +" "+c.count);
+        }
+    }
+}
